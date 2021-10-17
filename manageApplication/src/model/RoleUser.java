159bc4a975577c4a4d7baf953204e9d7d9b503b8
Rinku <sarkr72@mail.sunysuffolk.edu>
2021-10-16 21:39:40 -0400

    role user added

---
+++ manageApplication/src/model/RoleUser.java
@@ -0,0 +1,11 @@
+package model;
+
+public class RoleUser {
+	public RoleUser() {
+
+	}
+
+	public void showLabel() {
+
+	}
+}
--- manageApplication/src/model/User.java
+++ manageApplication/src/model/User.java
@@ -1,5 +1,19 @@
 package model;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Unitable, Serializable{
+	
+	private RoleUser roleUser;
+	private String userName;
+	private String passWord;
+	
+	public User(RoleUser roleUser) {
+		this.roleUser = roleUser;
+	}
+	
+	public void show() {
+		roleUser.showLabel();
+	}
 
 }
